4bd8aaa72f7561f9f510f8b4a4165b998a8a9ad0
Marcelocc22 <marcelo_cc22@outlook.com.br>
2022-09-17 19:58:18 -0300

    py5

---
+++ Python/exercicio3.py
@@ -0,0 +1,6 @@
+# Média de um Aluno
+
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+m = (n1 + n2) / 2
+print('A média do aluno é: {:.1f}'.format(m))
---
+++ Python/exercicio4.py
@@ -0,0 +1,11 @@
+# Conversor de medidas 
+
+medida = float(input('Escreva uma dsitância em Metros: '))
+cm = medida * 100
+mm = medida * 1000
+print('A medida {} em metros, correnponde a {}cm e {}mm '.format(medida, cm, mm))
+
+
+
+
+
